8a449413386cbb7625fc1422ede3993347503506
Pasha <zenoken4ik@gmail.com>
2023-03-01 23:23:02 +0200

    added extraReducers to weatherSlice

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,19 @@
+import { useDispatch, useSelector } from 'react-redux';
+import { useEffect } from 'react';
+import { getWeather } from './store/weather/weatherSlice';
+
 import CardWeather from "./components/CardWeather";
 import SearchBar from "./components/SearchBar";
 
 function App() {
+  const dispatch = useDispatch();
+  const { weather, isLoading } = useSelector((state) => state.weather);
+
+  useEffect(() => {
+    dispatch(getWeather())
+
+  }, [dispatch]);
+  
   return (
     <div className='container text-center '>
     <SearchBar />
--- client/src/store/weather/weatherSlice.js
+++ client/src/store/weather/weatherSlice.js
@@ -3,7 +3,7 @@ import weatherService from '../services/weatherService';
 
 export const getWeather = createAsyncThunk('GET_WEATHER', async (_, thunkAPI) => {
     try {
-        return await weatherService.getRepos();
+        return await weatherService.getWeather();
 
     } catch (error) {
         return thunkAPI.rejectWithValue(error.response.data)
@@ -12,10 +12,27 @@ export const getWeather = createAsyncThunk('GET_WEATHER', async (_, thunkAPI) =>
 const weatherSlice = createSlice({
     name: 'weather',
     initialState: {
-        weather: null,
+        weatherCity: null,
         isError: false,
         isLoading: false,
         message: ''
+    },
+    extraReducers: (builder) => {
+        builder.addCase(getWeather.pending, (state) => {
+            state.isLoading = true
+        });
+
+        builder.addCase(getWeather.fulfilled, (state, action) => {
+            state.isLoading = false;
+            state.weatherCity = action.payload
+        });
+
+        builder.addCase(getWeather.rejected, (state, action) => {
+            state.isLoading = false;
+            state.isError = true;
+            state.message = action.payload.message;
+            state.weatherCity = null;
+        });
     }
 });
 
